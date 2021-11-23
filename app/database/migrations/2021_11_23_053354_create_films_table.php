9076b6c5e44cb0a315b902a433ab6ce89d381a7d
Shulha Sergey <sv_shulga@ukr.net>
2021-11-23 08:24:45 +0200

    last changes

--- app/database/migrations/2021_11_23_053354_create_films_table.php
+++ app/database/migrations/2021_11_23_053354_create_films_table.php
@@ -16,6 +16,7 @@ class CreateFilmsTable extends Migration
         Schema::create('films', function (Blueprint $table) {
             $table->id();
             $table->string("film_name", 255);
+            $table->string("preview_url", 255);
             $table->integer("publish");
         });
     }
--- app/database/seeders/FilmsTableSeeder.php
+++ app/database/seeders/FilmsTableSeeder.php
@@ -14,31 +14,34 @@ class FilmsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('films')->delete();
-        
+
         \DB::table('films')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'film_name' => 'The dog "Richi"',
+                'preview_url' => '/storage/app/public/films_img/1.jpg',
                 'publish' => 0,
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
                 'film_name' => 'Sky in orange',
+                'preview_url' => '/storage/app/public/films_img/2.jpg',
                 'publish' => 0,
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 3,
                 'film_name' => 'Shark',
+                'preview_url' => '/storage/app/public/films_img/3.jpg',
                 'publish' => 0,
             ),
         ));
-        
-        
+
+
     }
 }
